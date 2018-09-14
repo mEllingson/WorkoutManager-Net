274175f321e886511e6b6cbcfc78661e29b9e242
mEllingson <mitch.w.ellingson@gmail.com>
2018-09-14 10:35:35 -0500

    Initial commit of new project.

---
+++ src/WorkoutManager/Models/ExerciseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkoutManager.Models
+{
+    public class ExerciseType
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
