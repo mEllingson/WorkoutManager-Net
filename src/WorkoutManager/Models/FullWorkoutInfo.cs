edeb584c953b37450b46ad17356a5bb574addec5
mEllingson <mitch.w.ellingson@gmail.com>
2018-09-28 13:42:58 -0500

    Added base model for full workout plan info.

---
+++ src/WorkoutManager/Models/FullWorkoutInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkoutManager.Models
+{
+    public class FullWorkoutInfo
+    {
+        //workoutDayTemplateID = 1, 
+        //    workoutDayTemplateName = "Overhead Press Day", 
+        //    id = 1, 
+        //    exercise = "Overhead Press", 
+        //    exerciseType = "Primary", 
+        //    reps = 5, 
+        //    useTM = true, 
+        //    tmPercent = 40, 
+        //    amrapSet = false,
+        //    warmupSet = true 
+}
+}
